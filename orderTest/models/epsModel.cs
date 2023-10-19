8de7e3b36f55f13e4735b7ae4af4d92559bc0659
yurkiss <terranet23@gmail.com>
2023-10-19 15:20:19 +0300

    20231019 add files model, viewmodel, command

---
+++ orderTest/models/epsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orderTest.models
+{
+    internal class epsModel
+    {
+        public string Mark { get; set; }
+        public int Thikness { get; set; }
+        public double Amount { get; set; }
+        public int Pack { get; set; }
+    }
+}
