9ebfce94f7adf55cc405c3a3ff4b84b33cbee1e6
yurkiss <terranet23@gmail.com>
2023-12-20 17:31:54 +0200

    20231220 end stolit

--- orderTest/panels/EpsPanel.cs
+++ orderTest/panels/EpsPanel.cs
@@ -15,16 +15,16 @@ namespace orderTest
         private List<epsModel> EpsList = new List<epsModel>();
         private List<string> removeEPS = new List<string>();
         private List<string> editEPS = new List<string>();
-        private List<int> epsCltInd = new List<int>();
+        private List<int> epsInd = new List<int>();
         private string[] addEPS;
 
         private void markEPS_SelectedIndexChanged(object sender, EventArgs e) => isNullPosition(markEPS, thikEPS);
 
         private void thikEPS_SelectedIndexChanged(object sender, EventArgs e) => isNullPosition(thikEPS, amountEPS);
 
-        private void amountEPS_TextChanged(object sender, EventArgs e) => isNum((TextBox)sender, new[] { addEpsToOrderButton }, packEPS);
+        private void amountEPS_TextChanged(object sender, EventArgs e) => isNum((TextBox)sender, [addEpsToOrderButton], packEPS);
 
-        private void amountEPS_Leave(object sender, EventArgs e) { fillEnable(new Control[] { packEPS, addEpsToOrderButton }, true); calcPack(packEPS); }
+        private void amountEPS_Leave(object sender, EventArgs e) { fillEnable([packEPS, addEpsToOrderButton], true); calcPack(packEPS); }
 
         private void addEpsToOrderButton_EnabledChanged(object sender, EventArgs e) => isButtonStateChanged((Button)sender);
 
@@ -32,10 +32,10 @@ namespace orderTest
         {
             //новий рядок
             addEPS = [markEPS.SelectedItem.ToString(), thikEPS.SelectedItem.ToString(), amountEPS.Text, packEPS.Text];
-            epsModel eps = new epsModel(addEPS);
 
             //додаємо eps в замовлення
-            EpsList.Add(eps); txtBold(radioEPS); radioEPS.ForeColor = Color.DarkGreen; if (clients > 1) epsCltInd.Add(cList.SelectedIndex);
+            EpsList.Add(new(addEPS)); txtBold(radioEPS); radioEPS.ForeColor = Color.DarkGreen;
+            if (clients > 1) epsInd.Add(cList.SelectedIndex);
 
             //додаємо рядок в таблицю на формі
             epsData.Rows.Add(addEPS); epsData.Visible = true;
@@ -49,15 +49,15 @@ namespace orderTest
 
         private void epsDelete_Click(object sender, EventArgs e)
         {
-            MessageBoxResult result = System.Windows.MessageBox.Show("справді видалити?", "видалити рядок", MessageBoxButton.OKCancel);
+            showResult();
+            //MessageBoxResult result = System.Windows.MessageBox.Show("справді видалити?", "видалити рядок", MessageBoxButton.OKCancel);
 
             //рядок з таблиці, який видаляємо
             foreach (DataGridViewCell item in epsData.SelectedRows[0].Cells) removeEPS.Add(item.Value.ToString());
 
             //видаляємо eps із замовлення
             epsModel remove = new epsModel(removeEPS.ToArray()) { }; int i = EpsList.FindIndex(e => e.Mark == remove.Mark && e.Thikness == remove.Thikness && e.Amount == remove.Amount);
-            EpsList.RemoveAt(i);
-            if (clients > 1) epsCltInd.RemoveAt(i);
+            EpsList.RemoveAt(i); if (clients > 1) epsInd.RemoveAt(i);
             //EpsList.RemoveAll(e => e.Mark == remove.Mark && e.Thikness == remove.Thikness && e.Amount == remove.Amount);
 
             //видаляємо рядок з таблиці на формі
